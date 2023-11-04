23e1f541fbda494cdd35d15dd3ce3bd3b0e1de6b
Thomas Dunlop <dunlopt12@gmail.com>
2023-11-04 17:41:38 -0400

    first models

--- backend/task_management/models.py
+++ backend/task_management/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskBadge(models.Model):
+    name = models.TextField()
+
+
+class TaskOption(models.Model):
+    name = models.TextField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    duration = models.FloatField()
+    badges = models.ManyToManyField(TaskBadge)
+
+
+class Task(models.Model):
+    complete = models.BooleanField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    priority = models.BigIntegerField()
+    option = models.ForeignKey(TaskOption, on_delete=models.CASCADE)
